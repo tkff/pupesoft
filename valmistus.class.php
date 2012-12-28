3d7825001f79b1a92f0d7fd7699294d12b9f54fa
Antti Jäppinen <antti@devlab.fi>
2012-12-28 16:47:36 +0200

    Valmistuksen tila muutokset

--- valmistus.class.php
+++ valmistus.class.php
@@ -5,7 +5,7 @@
 */
 class Valmistus {
 
-	// Pakolliset kentÃ¤t
+	// Pakolliset kentät
 	private $yhtio;
 	private $tunnus;
 
@@ -50,7 +50,7 @@ class Valmistus {
 		return $this->tuotteet;
 	}
 
-	/** etsitÃ¤Ã¤n valmistuksen puutteet
+	/** etsitään valmistuksen puutteet
 	*/
 	function puutteet() {
 		return "Puutteet";
@@ -98,15 +98,48 @@ class Valmistus {
 	###
 	function edellinen() {
 		if ($this->valmistuslinja != '') {
-			// EtitÃ¤Ã¤n edellinen valmistus
+			// Etitään edellinen valmistus
 			# return new Valmistus($tunnus);
 		}
 	}
 
 	function seuraava() {
 		if ($this->valmistuslinja != '') {
-			// EtitÃ¤Ã¤n seuraava valmistus
+			// Etitään seuraava valmistus
 			# return new Valmistus($tunnus);
+		}
+	}
+
+	/** Päivittää valmistuksen tilan
+	*/
+	function tila($tila) {
+		global $kukarow;
+
+		/** Sallitut tilat ja niiden mahdolliset vaihtoehdot
+		* OV => Odottaa valmistusta,
+		* VA => Valmistuksessa,
+		* TK => Työ keskeytetty,
+		* VT => Valmis tarkastukseen,
+		* TA => Tarkistettu
+		*/
+		$states = array(
+			'OV' => array('VA'),
+			'VA' => array('TK', 'VT'),
+			'TK' => array('VA'),
+			'VT' => array('TA', 'OV')
+			);
+
+		// Voidaanko uuteen tilaan vaihtaa,
+		// eli löytyykö haluttu tila nykyisen tilan vaihtoehdoista.
+		if (in_array($tila, $states[$this->tila]))  {
+
+			// TODO: tarkistetaan mikä tila on nyt jne...
+			$query = "UPDATE lasku SET valmistuksen_tila='$tila' WHERE yhtio='{$kukarow['yhtio']}' AND tunnus=$this->tunnus";
+			echo $query;
+			#$result = pupe_query($query);
+		}
+		else {
+			exit("Laiton tilan muutos");
 		}
 	}
 
@@ -131,6 +164,31 @@ class Valmistus {
 		}
 
 		return $valmistukset;
+	}
+
+	/** Hakee yksittäisen valmistuksen */
+	static function find($tunnus) {
+		global $kukarow;
+
+		$query = "SELECT
+						lasku.yhtio,
+						lasku.tunnus,
+						lasku.valmistuksen_tila as tila,
+						kalenteri.henkilo as valmistuslinja
+					FROM lasku
+					LEFT JOIn kalenteri on (lasku.yhtio=kalenteri.yhtio AND lasku.tunnus=kalenteri.otunnus)
+					WHERE lasku.yhtio='{$kukarow['yhtio']}'
+					AND lasku.tunnus=$tunnus LIMIT 1";
+		$result = pupe_query($query);
+
+		if ($valmistus = mysql_fetch_object($result, 'valmistus')) {
+			return $valmistus;
+		}
+		else {
+			return false;
+		}
+
+
 	}
 }
 
